7c7a0b93644ef8b30899008badc5ecdd6d883dfb
andre-cyb <andrea.vg.malfatti@gmail.com>
2022-01-26 23:37:43 +0100

    creato nuovo progetto laravel-boolpress

---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-center">benvenuto</h1>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  name: "app",
+  data() {
+    return {};
+  },
+};
+</script>
